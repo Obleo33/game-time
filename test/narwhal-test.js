7b5978f35659ca4b705669ee545318a8d352aebe
Franklin Crosby <obleo33@gmail.com>
2017-01-08 09:41:20 -0700

    Add initial tests for narwhal and seal

--- test/narwhal-test.js
+++ test/narwhal-test.js
@@ -1,3 +1,19 @@
 var chai = require('chai');
 var assert = chai.assert;
+
 var Narwhal = require('../lib/narwhal');
+
+describe('Narwhal', function() {
+  context('with default attributes', function() {
+
+    var narwhal = new Narwhal({});
+    
+    it('should be a function', function () {
+      assert.isFunction(Narwhal);
+    });
+
+    it('should instantiate a Narwhal', function () {
+      assert.isObject(narwhal);
+    });
+  });
+});
