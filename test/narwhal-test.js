b22505931bbdb4c76c7bc9cb250467584a859ee2
Julian Kyer <julian.kyer@gmail.com>
2017-01-10 10:03:47 -0700

    Add narwhal tests

--- test/narwhal-test.js
+++ test/narwhal-test.js
@@ -7,7 +7,7 @@ describe('Narwhal', function() {
   context('with default attributes', function() {
 
     var narwhal = new Narwhal({});
-    
+
     it('should be a function', function () {
       assert.isFunction(Narwhal);
     });
@@ -15,5 +15,40 @@ describe('Narwhal', function() {
     it('should instantiate a Narwhal', function () {
       assert.isObject(narwhal);
     });
+
+    it('should have a default speed', function() {
+      assert.equal(narwhal.speed, 3);
+    });
+
+    it('should not have a burst of speed by default', function() {
+      assert.equal(narwhal.flipperKick, false);
+    });
+
+    it('should have a function "updateNarwhal" that updates the narwhals position', function() {
+      assert.isFunction(narwhal.updateNarwhal);
+    });
+
+    it('should have a property called moveLeft that defaults to false', function() {
+      assert.equal(narwhal.moveLeft, 432);
+
+    });
+
+    it.skip('if "moveLeft" is true, narwhal.x should decrement by its speed', function() {
+      narwhal.moveLeft = true;
+      assert.equal(narwhal.moveLeft, true);
+      assert.equal(narwhal.x, 432)
+    });
+
+    it.skip('if moveRight is true, narwhal.x should increment by its speed', function() {
+
+    });
+
+    it.skip('if flipperKick is true, narwhal.y should decrement by 10', function() {
+
+    });
+
+    it.skip('')
+
+
   });
 });
