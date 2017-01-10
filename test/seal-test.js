bda8a5ae7135fdd5f42a58a5c098975a7fede338
Franklin Crosby <obleo33@gmail.com>
2017-01-10 14:34:56 -0700

    Begin testing seals

--- test/seal-test.js
+++ test/seal-test.js
@@ -6,14 +6,32 @@ var Seal = require('../lib/seal');
 describe('Seal', function() {
   context('with default attributes', function() {
 
-    var seal = new Seal({});
-
     it('should be a function', function () {
       assert.isFunction(Seal);
     });
 
     it('should instantiate a Seal', function () {
+      var seal = new Seal();
       assert.isObject(seal);
     });
+
+    it('should have a move function',function(){
+      var seal = new Seal();
+      assert.isFunction(seal.move);
+    })
+
+    it('should move the value of speed in the x direction', function (){
+      var seal = new Seal();
+      var speed = seal.speed
+      var x = seal.x
+      seal.move()
+      assert.equal(seal.x, x + speed)
+    });
+
+    it('should change direction if the x value is greather than the canvas width',function(){
+
+    })
+
+
   });
 });
