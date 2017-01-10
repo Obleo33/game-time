0a8759504c4f4e0a10bcdeaa264605e05b78e76b
Julian Kyer <julian.kyer@gmail.com>
2017-01-10 12:16:15 -0700

    Add narwhal size, movement and collision tests

--- test/narwhal-test.js
+++ test/narwhal-test.js
@@ -6,49 +6,153 @@ var Narwhal = require('../lib/narwhal');
 describe('Narwhal', function() {
   context('with default attributes', function() {
 
-    var narwhal = new Narwhal({});
+    // var narwhal = new Narwhal({});
 
     it('should be a function', function () {
       assert.isFunction(Narwhal);
     });
 
     it('should instantiate a Narwhal', function () {
+      var narwhal = new Narwhal();
       assert.isObject(narwhal);
     });
 
+    it('should have a default x and y position for body and tusk', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.x, 435);
+      assert.equal(narwhal.y, 250);
+      assert.equal(narwhal.tuskX, 450);
+      assert.equal(narwhal.tuskY, 160);
+    });
+
+    it('should have a default body height and width', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.height, 40);
+      assert.equal(narwhal.width, 30);
+    });
+
+    it('should have a default tusk height and width', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.tuskHeight, 90);
+      assert.equal(narwhal.tuskWidth, 1);
+    });
+
     it('should have a default speed', function() {
+      var narwhal = new Narwhal();
       assert.equal(narwhal.speed, 3);
     });
 
     it('should not have a burst of speed by default', function() {
+      var narwhal = new Narwhal();
       assert.equal(narwhal.flipperKick, false);
     });
 
-    it('should have a function "updateNarwhal" that updates the narwhals position', function() {
+    it('should have a function "updateNarwhal"', function() {
+      var narwhal = new Narwhal();
       assert.isFunction(narwhal.updateNarwhal);
     });
 
     it('should have a property called moveLeft that defaults to false', function() {
-      assert.equal(narwhal.moveLeft, 432);
-
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.moveLeft, false);
     });
 
-    it.skip('if "moveLeft" is true, narwhal.x should decrement by its speed', function() {
+    it('if moveLeft is true, x should decrement by narwhal speed', function(){
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.x, 435);
       narwhal.moveLeft = true;
-      assert.equal(narwhal.moveLeft, true);
-      assert.equal(narwhal.x, 432)
+      narwhal.updateNarwhal();
+      assert.equal(narwhal.x, 432);
     });
 
-    it.skip('if moveRight is true, narwhal.x should increment by its speed', function() {
-
+    it('should have a property moveRight that defaults to false', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.moveRight, false);
     });
 
-    it.skip('if flipperKick is true, narwhal.y should decrement by 10', function() {
-
+    it('if moveRight is true, x should increment by narwhal speed',function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.x, 435);
+      narwhal.moveRight = true;
+      narwhal.updateNarwhal();
+      assert.equal(narwhal.x, 438);
     });
 
-    it.skip('')
+    it('should have a property flipperKick that defaults to false', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.flipperKick, false);
+    });
 
+    it('if flipperKick is true, narwhal should move up by its speed', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.y, 250);
+      narwhal.flipperKick = true;
+      narwhal.updateNarwhal();
+      assert.equal(narwhal.y, 247);
+    });
+
+    it('should have a property burst that defaults to false', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.burst, false);
+    });
+
+    it('if burst is true, narwhal should move up 10x its speed', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.y, 250);
+      narwhal.burst = true;
+      narwhal.updateNarwhal();
+      assert.equal(narwhal.y, 220);
+    });
+
+    it('should have a detectCollision function', function() {
+      var narwhal = new Narwhal();
+      assert.isFunction(narwhal.detectCollision);
+    });
+
+    function swimLeft(num, narwhal) {
+      for (var i = 0; i < num; i++) {
+        narwhal.moveLeft = true;
+        narwhal.updateNarwhal();
+      }
+    }
+
+    it('detectCollision should prevent narwhal moving left of the canvas edge', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.x, 435);
+      swimLeft(440, narwhal);
+      narwhal.detectCollision();
+      assert.equal(narwhal.x, 0);
+    });
+
+    function swimRight(num, narwhal) {
+      for (var i = 0; i < num; i++) {
+        narwhal.moveRight = true;
+        narwhal.updateNarwhal();
+      }
+    }
+
+    it('detectCollision should prevent narwhal moving right of the canvas edge', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.x, 435);
+      swimRight(915, narwhal);
+      narwhal.detectCollision();
+      assert.equal(narwhal.x, 900);
+    });
+
+    function swimUp(num, narwhal) {
+      for (var i = 0; i < num; i++) {
+        narwhal.flipperKick = true;
+        narwhal.updateNarwhal();
+      }
+    }
+
+    it('detectCollision should prevent narwhal moving above the ice', function() {
+      var narwhal = new Narwhal();
+      assert.equal(narwhal.y, 250);
+      swimUp(175, narwhal);
+      narwhal.detectCollision();
+      assert.equal(narwhal.y, 125);
+    });
 
   });
 });
